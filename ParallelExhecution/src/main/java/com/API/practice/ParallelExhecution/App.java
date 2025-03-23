9378b56ddd491399ff96f06ebcd4944dd7bbad7e
pritamhoro <pritam.horo143@gmail.com>
2025-03-23 15:30:56 +0530

    Add parallel processing support for improved performance

---
+++ ParallelExhecution/src/main/java/com/API/practice/ParallelExhecution/App.java
@@ -0,0 +1,10 @@
+package com.API.practice.ParallelExhecution;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
