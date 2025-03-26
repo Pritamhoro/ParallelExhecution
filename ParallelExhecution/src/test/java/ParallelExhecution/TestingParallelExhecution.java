1c8865c4e5ab5027fadf4832ff2399f388a12e99
pritamhoro <pritam.horo143@gmail.com>
2025-03-26 20:19:13 +0530

    Add parallel processing support for improved performance

--- ParallelExhecution/src/test/java/ParallelExhecution/TestingParallelExhecution.java
+++ ParallelExhecution/src/test/java/ParallelExhecution/TestingParallelExhecution.java
@@ -1,7 +1,11 @@
 package ParallelExhecution;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class TestingParallelExhecution 
@@ -9,6 +13,8 @@ public class TestingParallelExhecution
 	@Test
 	public void Shopping()
 	{
+		ChromeOptions ch=new ChromeOptions();
+		ch.addArguments("--headless");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/client");
@@ -18,9 +24,23 @@ public class TestingParallelExhecution
 	@Test
 	public void Banking()
 	{
+		ChromeOptions ch=new ChromeOptions();
+		ch.addArguments("--headless");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://swift.techwithjatin.com/login");
 		driver.close();
 	}
+	@Test
+	public void Movie()
+	{
+		ChromeOptions ch=new ChromeOptions();
+		ch.addArguments("--headless");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://mkvcinemas.kim/");
+		driver.close();
+
+	}
+
 }
